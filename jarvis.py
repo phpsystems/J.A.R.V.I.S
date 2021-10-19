d8cf0e9522e6c20396ab24a51b6b038afba64fd3
phpsystems <phpsystems@users.noreply.github.com>
2021-10-19 19:53:21 +0100

    Update jarvis.py
    update to add placeholder for the options.

--- jarvis.py
+++ jarvis.py
@@ -7,12 +7,21 @@ JARVIS - Just Another Really Very Intelligent System
 import daemon
 import time
 import RPi.GPIO as GPIO
+import optparse
 
 def main():
   print "Starting AI..."
 
 if __name__ == "__main__":
 # Options for the command line.
+  # Debug
+	
+	# Configuration file
+
+	
+	
+	# Print start up message
+	print "Starting AI..."
   
   if ():
     with daemon.DaemonContext():
