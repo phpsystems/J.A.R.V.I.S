1cc370eb9aaf029d7f29a4d9c84f52da8d773c31
phpsystems <phpsystems@users.noreply.github.com>
2021-10-19 19:48:44 +0100

    Create jarvis.py
    Initial program commit.

---
+++ jarvis.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+'''
+JARVIS - Just Another Really Very Intelligent System
+'''
+
+import daemon
+import time
+import RPi.GPIO as GPIO
+
+def main():
+  print "Starting AI..."
+
+if __name__ == "__main__":
+# Options for the command line.
+  
+  if ():
+    with daemon.DaemonContext():
+        main()
+  else:
+    # Run the main program in the foreground.
+    main()
