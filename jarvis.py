c5d49b58046b21650aa08da8447174927d9f55f0
Lizzy Higgins <lizzyhiggins@Lizzys-MBP.php-systems.com>
2021-10-19 23:14:35 +0100

    update the main program

--- jarvis.py
+++ jarvis.py
@@ -8,23 +8,29 @@ JARVIS - Just Another Really Very Intelligent System
 import daemon
 import time
 import RPi.GPIO as GPIO
-import optparse
+from optparse import OptionParser
 
 def main():
   print "Starting AI..."
 
 if __name__ == "__main__":
+  parser=OptionParser()
+
 # Options for the command line.
+
   # Debug
+  parser.add_option ("-v", action="store_true", dest="verbose")
+  parser.add_option ("-d", action="store_true", dest="rundaemon")
 	
 	# Configuration file
 
 	
-	
+	(options,args)=parser.parse_args()
+  
 	# Print start up message
 	print "Starting AI..."
   
-  if ():
+  if (rundaemon is true):
     with daemon.DaemonContext():
         main()
   else:
